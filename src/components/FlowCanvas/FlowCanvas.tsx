04713058f2a31af0fa6a4f805084843b9a871db7
JonaLGarza <jonathanlgarza@outlook.com>
2025-04-24 00:12:56 -0600

    feat(FlowCanvas): added tableNode to flowCanvas

--- src/components/FlowCanvas/FlowCanvas.tsx
+++ src/components/FlowCanvas/FlowCanvas.tsx
@@ -2,10 +2,9 @@ import ReactFlow, {
   Background,
   Connection,
   Controls,
-  Edge,
   MiniMap,
   addEdge,
-  Node,
+  Node
 } from "reactflow";
 import useFlowData from "../hooks/useFlowData";
 import { generateNode } from "../utils/nodeHelpers";
@@ -13,16 +12,24 @@ import { nodeTypes } from "../../nodeTypes";
 
 function FlowCanvas() {
   const { nodes, setNodes, onNodesChange, edges, setEdges, onEdgesChange } = useFlowData();
-  const onConnect = (params: Connection) => setEdges((eds: Edge[]) => addEdge(params, eds));
-  const addNewNode = () => setNodes((nds: Node[]) => [...nds, generateNode(nds.length, "custom")]);
+  const onConnect = (params: Connection) => setEdges(addEdge(params, edges));
+  const addNewNode = (type = "custom") => {
+    setNodes((nds: Node[]) => [...nds, generateNode(nds.length, type)]);
+  };
 
   return (
     <div style={{ width: "100vw", height: "100vh" }}>
       <button
-        onClick={addNewNode}
+        onClick={() => addNewNode("custom")}
         style={{ position: "absolute", zIndex: 10, top: 10, left: 10 }}
       >
         Add Node
+      </button>
+      <button
+        onClick={() => addNewNode("table")}
+        style={{ position: "absolute", zIndex: 10, top: 60, left: 10 }}
+      >
+        Add Table
       </button>
       <ReactFlow
         nodes={nodes}
