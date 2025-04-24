7a3ba0323cefc4a4d833ffc66e0bb54f1725aa5c
JonaLGarza <jonathanlgarza@outlook.com>
2025-04-24 00:11:22 -0600

    feat(nodeHelpers): added baseData rows to generateNode

--- src/components/utils/nodeHelpers.tsx
+++ src/components/utils/nodeHelpers.tsx
@@ -1,12 +1,28 @@
-export const generateNode = (id: number, type: string = "custom") => {
-    return {
-      id: (id + 1).toString(),
-      type,
-      data: { label: `Node ${id + 1}` },
-      position: {
-        x: Math.random() * 400,
-        y: Math.random() * 400,
-      },
-    };
+import { Node } from "reactflow";
+
+interface IBaseNodeData {
+  label: string;
+  rows?: { key: string; value: string; }[];
+}
+
+export const generateNode = (id: number, type: string = "custom"): Node<IBaseNodeData> => {
+  const baseData: IBaseNodeData = { 
+    label: `Node ${id + 1}` 
   };
+  if (type === "table") {
+    baseData.rows = [
+      { key: "Item 1", value: "100" },
+      { key: "Item 2", value: "200" },
+    ];
+  }
+  return {
+    id: (id + 1).toString(),
+    type,
+    data: baseData,
+    position: {
+      x: Math.random() * 400,
+      y: Math.random() * 400,
+    },
+  };
+};
   
