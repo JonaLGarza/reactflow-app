b9f49cf5a0d714185e4daf7800ecfb65f7292e7b
JonaLGarza <jonathanlgarza@outlook.com>
2025-04-23 21:52:12 -0600

    feat(FlowCanvas): refactored file location

---
+++ src/components/FlowCanvas/FlowCanvas.tsx
@@ -0,0 +1,42 @@
+import ReactFlow, {
+  Background,
+  Connection,
+  Controls,
+  Edge,
+  MiniMap,
+  addEdge,
+  Node,
+} from "reactflow";
+import useFlowData from "../hooks/useFlowData";
+import { generateNode } from "../utils/nodeHelpers";
+
+function FlowCanvas() {
+  const { nodes, setNodes, onNodesChange, edges, setEdges, onEdgesChange } = useFlowData();
+  const onConnect = (params: Connection) => setEdges((eds: Edge[]) => addEdge(params, eds));
+  const addNewNode = () => setNodes((nds: Node[]) => [...nds, generateNode(nds.length)]);
+
+  return (
+    <div style={{ width: "100vw", height: "100vh" }}>
+      <button
+        onClick={addNewNode}
+        style={{ position: "absolute", zIndex: 10, top: 10, left: 10 }}
+      >
+        Add Node
+      </button>
+      <ReactFlow
+        nodes={nodes}
+        edges={edges}
+        onNodesChange={onNodesChange}
+        onEdgesChange={onEdgesChange}
+        onConnect={onConnect}
+        fitView
+      >
+        <MiniMap />
+        <Controls />
+        <Background color="#aaa" gap={16} />
+      </ReactFlow>
+    </div>
+  );
+}
+
+export default FlowCanvas;
