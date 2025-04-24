3eb5d7be6f7d18445b7946eba972c32e4ac0e639
JonaLGarza <jonathanlgarza@outlook.com>
2025-04-24 00:10:21 -0600

    feat(nodeTypes): added tableNode to nodeTypes

--- src/nodeTypes/index.tsx
+++ src/nodeTypes/index.tsx
@@ -1,6 +1,8 @@
 import CustomLogicNode from "../components/CustomLogicNode/CustomLogicNode";
+import TableNode from "../components/TableNode/TableNode";
 
 
 export const nodeTypes = {
   custom: CustomLogicNode,
+  table: TableNode,
 };
