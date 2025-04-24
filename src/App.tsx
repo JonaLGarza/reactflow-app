e892ab5761111ed93da4d8eef6c42047f4fe7bac
JonaLGarza <jonathanlgarza@outlook.com>
2025-04-23 21:54:06 -0600

    feat(App): refactored app file to use refactored components

--- src/App.tsx
+++ src/App.tsx
@@ -1,52 +1,7 @@
 import ReactDOM from "react-dom/client";
-import ReactFlow, {
-  Background,
-  Connection,
-  Controls,
-  MiniMap,
-  ReactFlowProvider,
-  addEdge,
-  useEdgesState,
-  useNodesState,
-} from "reactflow";
+import { ReactFlowProvider } from "reactflow";
 import "reactflow/dist/style.css";
-
-const initialNodes = [
-  {
-    id: "1",
-    type: "default",
-    data: { label: "Start Node" },
-    position: { x: 250, y: 5 },
-  },
-];
-
-const initialEdges: Edge[] = [];
-
-function FlowCanvas() {
-
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes);
-  const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges);
-
-  const onConnect = (params: Connection) => setEdges((eds) => addEdge(params, eds));
-
-  return (
-    <div style={{ width: "100vw", height: "100vh" }}>
-      <ReactFlow
-        nodes={nodes}
-        edges={edges}
-        onNodesChange={onNodesChange}
-        onEdgesChange={onEdgesChange}
-        onConnect={onConnect}
-        fitView
-      >
-        <MiniMap />
-        <Controls />
-        <Background color="#aaa" gap={16} />
-      </ReactFlow>
-    </div>
-  );
-}
+import FlowCanvas from "./components/FlowCanvas/FlowCanvas";
 
 export default function App() {
   return (
